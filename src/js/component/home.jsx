fa6e604c14ce060b500625fd6a2a6f4da3f48852
Marcos Cabral Peralta <156668187+Marcos-93LAB@users.noreply.github.com>
2024-03-22 20:29:37 +0000

    primer commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,21 +6,38 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container-fluid bg-dark">
+			<div className="container">
+				<nav className="navbar navbar-expand-lg navbar-light bg-dark">
+					<a className="navbar-brand text-white" href="#">Start Bootstrap</a>
+					<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+						<span className="navbar-toggler-icon"></span>
+					</button>
+					
+						<div className="collapse navbar-collapse" id="navbarNav">
+							<ul className="navbar-nav ml-auto">
+							<li className="nav-item active">
+								<a className="nav-link text-white" href="#">Home <span class="sr-only">(current)</span></a>
+							</li>
+							<li className="nav-item">
+								<a className="nav-link text-white" href="#">About</a>
+							</li>
+							<li className="nav-item">
+								<a className="nav-link text-white" href="#">Service</a>
+							</li>
+							<li className="nav-item">
+								<a className="nav-link text-white">Contact</a>
+							</li>
+							</ul>
+						</div>
+					
+				</nav>
+			</div>
 		</div>
 	);
 };
 
+
+
 export default Home;
+
